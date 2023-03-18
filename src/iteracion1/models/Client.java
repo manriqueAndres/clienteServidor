07269eaaf42bac8ab56f27c51b4ee1709ba67d24
manriqueAndres <93721638+manriqueAndres@users.noreply.github.com>
2023-03-17 21:37:53 -0500

    Update Client.java

--- src/iteracion1/models/Client.java
+++ src/iteracion1/models/Client.java
@@ -2,36 +2,37 @@ package iteracion1.models;
 
 public class Client extends Persona{
 
-	public Client(String name, String lastNames, String id, String password, String email) {
-		super(name, lastNames, id, password, email);
-	}
+    public Client(String name, String lastNames, String id, String password, String email) {
+        super(name, lastNames, id, password, email);
+    }
 
-	public boolean isNameEqualTo(String names) {
-		if(getName().equals(names))
-			return true;
-		return false;
-	}
+    // Verifica si el nombre del cliente es igual al nombre dado
+    public boolean isNameEqualTo(String names) {
+        if(getName().equals(names))
+            return true;
+        return false;
+    }
 
-	public void updateAttribute(String attributeToChange, String newValueToAtrribute) {
-		attributeToChange = attributeToChange.toLowerCase();
-		
-		switch (attributeToChange) {
-		case "nombres":
-			setName(newValueToAtrribute);
-			break;
-		case "apellidos":
-			setLastNames(newValueToAtrribute);
-			break;
-		default:
-			break;
-		}
+    // Actualiza el atributo del cliente dado con el nuevo valor proporcionado
+    public void updateAttribute(String attributeToChange, String newValueToAtrribute) {
+        attributeToChange = attributeToChange.toLowerCase();
+        
+        switch (attributeToChange) {
+        case "nombres":
+            setName(newValueToAtrribute);
+            break;
+        case "apellidos":
+            setLastNames(newValueToAtrribute);
+            break;
+        default:
+            break;
+        }
+    }
 
-		
-	}
-
-	public boolean isPasswordEqualTo(String password) {
-		if(getPassword().equals(password))
-			return true;
-		return false;
-	}
+    // Verifica si la contraseña del cliente es igual a la contraseña dada
+    public boolean isPasswordEqualTo(String password) {
+        if(getPassword().equals(password))
+            return true;
+        return false;
+    }
 }
