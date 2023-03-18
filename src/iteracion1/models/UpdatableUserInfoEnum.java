115f2c4901d26c012973baf335fd4fa7730ed93e
manriqueAndres <93721638+manriqueAndres@users.noreply.github.com>
2023-03-17 21:43:53 -0500

    Update UpdatableUserInfoEnum.java

--- src/iteracion1/models/UpdatableUserInfoEnum.java
+++ src/iteracion1/models/UpdatableUserInfoEnum.java
@@ -1,5 +1,7 @@
 package iteracion1.models;
 
+// Enumeración que define los tipos de atributos de un usuario que se pueden actualizar
 public enum UpdatableUserInfoEnum {
-	NOMBRES,APELLIDOS
+	NOMBRES, // nombre de usuario
+	APELLIDOS // apellidos de usuario
 }
